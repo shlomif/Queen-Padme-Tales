ad831eb6c3f9b3a907cded9b0be525e9f5474a6b
Shlomi Fish <shlomif@shlomifish.org>
2020-12-16 16:13:00 +0200

    start adding images

--- queen-padme-tales/screenplay/scripts/prepare-epub.pl
+++ queen-padme-tales/screenplay/scripts/prepare-epub.pl
@@ -5,25 +5,18 @@ use warnings;
 
 use utf8;
 
-use Shlomif::Screenplays::EPUB;
+use Shlomif::Screenplays::EPUB ();
 
 my $gfx = 'who-is-qoheleth.svg.png';
 
-my @images =
-(
+my @images = (
     $gfx,
-    'Friends-S02E04--Nothing-Sexier.svg.jpg',
-    'If-You-Wanna-Be-Sad.svg.jpg',
-    'sigourney-weaver--resized.jpg',
-    'summer-glau--two-guns--400w.jpg',
+    'tacos6306095903_a5c92746ba_o--tacos-reduced-by-jeffreyw--small-400px.webp',
 );
 
 my $obj = Shlomif::Screenplays::EPUB->new(
     {
-        images =>
-        {
-            (map { $_ => "images/$_", } @images),
-        },
+        images => { ( map { $_ => "images/$_", } @images ), },
     }
 );
 
@@ -35,33 +28,31 @@ $obj->run;
 
     $obj->output_json(
         {
-            data =>
-            {
+            data => {
                 filename => $epub_basename,
-                title => q{“So, Who the Hell is Qoheleth?”},
-                authors =>
-                [
+                title    => q{“So, Who the Hell is Qoheleth?”},
+                authors  => [
                     {
                         name => "Shlomi Fish",
                         sort => "Fish, Shlomi",
                     },
                 ],
-                contributors =>
-                [
+                contributors => [
                     {
                         name => "Shlomi Fish",
                         role => "oth",
                     },
                 ],
-                cover => "images/$gfx",
-                rights => "Creative Commons Attribution Noncommercial ShareAlike Unported (CC-by-nc-sa-3.0)",
-                publisher => 'http://www.shlomifish.org/',
-                language => 'en-GB',
-                subjects => [ 'FICTION/Humorous', 'Judaism', 'Bible', ],
-                identifier =>
-                {
+                cover  => "images/$gfx",
+                rights =>
+"Creative Commons Attribution Noncommercial ShareAlike Unported (CC-by-nc-sa-3.0)",
+                publisher  => 'http://www.shlomifish.org/',
+                language   => 'en-GB',
+                subjects   => [ 'FICTION/Humorous', 'Judaism', 'Bible', ],
+                identifier => {
                     scheme => 'URL',
-                    value => 'http://localhost/shlomif/homepage-local/humour/So-Who-The-Hell-Is-Qoheleth/',
+                    value  =>
+'http://localhost/shlomif/homepage-local/humour/So-Who-The-Hell-Is-Qoheleth/',
                 },
             },
         },
